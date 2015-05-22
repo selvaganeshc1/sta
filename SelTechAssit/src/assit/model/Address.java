5f1c2f59b18352ec495dac64a384bc2576d56b82
selvaganeshc1 <selvaganesh.c@justintegrateit.com>
2015-05-22 16:14:49 +0530

    system9 commit

--- SelTechAssit/src/assit/model/Address.java
+++ SelTechAssit/src/assit/model/Address.java
@@ -2,4 +2,14 @@ package assit.model;
 
 public class Address {
 
+	private long addressId;
+
+	public long getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(long addressId) {
+		this.addressId = addressId;
+	}
+	
 }
